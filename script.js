a5634b410da8e550882235c84cda399f50a32895
BarfGoblin <121530658+BarfGoblin@users.noreply.github.com>
2024-01-21 09:39:16 -0800

    script2.js

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let lazyloadImages;
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    const imageObserver = new IntersectionObserver((photos) => {
+      photos.forEach((entry) => {
+        if (entry.isIntersecting) {
+          let image = entry.target;
+          console.log("test");
+          image.src = image.dataset.src;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach((image) => {
+      imageObserver.observe(image);
+    });
+  } else {
+    let lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+
+    function lazyload() {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }
+
+      lazyloadThrottleTimeout = setTimeout(() => {
+        let scrollTop = window.scrollY;
+        lazyloadImages.forEach(function (img) {
+          if (img.offsetTop < window.innerHeight + scrollTop) {
+            img.src = img.dataset.src;
+            img.classList.remove("lazy");
+          }
+        });
+        if (lazyloadImages.length == 0) {
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  }
+});
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,5 +77,14 @@ function showSlides(n) {
   }
 
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
+
+//ARROW KEY FUNCTONALITY?
+// function checkKey(e) {
+//   e = e || window.event;
+//   if (e.keyCode == "37") {
+//     // left arrow
+//   } else if (e.keyCode == "39") {
+//     // right arrow
+//   }
+// }
