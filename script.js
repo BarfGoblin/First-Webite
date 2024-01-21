bb524d0563d6480811d8700c67ae972a080f4e7f
BarfGoblin <121530658+BarfGoblin@users.noreply.github.com>
2024-01-21 06:03:56 -0800

    script1.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      } else {
-        entry.target.classList.remove("show");
-      }
-    });
-  },
-  { rootMargin: "0px", threshold: [0, 0.1, 1] }
-);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const tags = document.querySelectorAll("img");
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-tags.forEach((tag) => {
-  observer.observe(tag);
-});
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
